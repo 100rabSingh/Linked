1e7d28f8d4b6db18812c5d18c28270f5cb48ee1d
100rabSingh <100rab2000@gmail.com>
2019-02-20 11:48:25 +0530

    some more changes

--- src/Linkedlist1.java
+++ src/Linkedlist1.java
@@ -1,3 +1,21 @@
 public class Linkedlist1 {
 
+    public int getCurrentSize(){
+        return currentSize;
+    }
+    public void setCurrentSize(int currentSize){
+        this.currentSize = currentSize;
+    }
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        currentSize = 0;
+    }
+
+    public static void main(String[] args){
+
+        MyLinkedList linkedList = new MyLinkedList();
+        Node node = new Node(10,null);
+
+    }
 }
