63ca3b38ff28296b7a7e735955f092afe78bbbaf
100rabSingh <100rab2000@gmail.com>
2019-02-20 11:28:40 +0530

    add Linkedlist Data,node

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,10 +1,29 @@
+import sun.awt.image.ImageWatched;
+
 public class LinkedList {
     private int data;
 
     public int getData(){
+
         return data;
     }
     public void setData(int data){
         this.data = data;
     }
+    public void setNext(LinkedList data)
+    {
+        this.next = next;
+    }
+
+    public LinkedList() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    public LinkedList(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public LinkedList(int data, )
 }
