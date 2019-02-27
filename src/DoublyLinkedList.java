35f86942d84baecb4f4742e4c10fcf5f1e934bc7
100rabSingh <100rab2000@gmail.com>
2019-02-27 10:43:40 +0530

    Constructor of Node class

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,2 +1,14 @@
 public class DoublyLinkedList {
+    private static class Node{
+        private int data;
+        private Node pre;
+        private Node next;
+
+        public Node(int data, Node pre, Node next) {
+            this.data = data;
+            this.pre = pre;
+            this.next = next;
+        }
+
+    }
 }
