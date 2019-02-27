cf25cf5a5351f445fff4a92b6c40ea9c3d05fc55
100rabSingh <100rab2000@gmail.com>
2019-02-27 10:47:31 +0530

    override method

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,5 +1,5 @@
 public class DoublyLinkedList {
-    private static class Node{
+    private static class Node {
         private int data;
         private Node pre;
         private Node next;
@@ -9,13 +9,15 @@ public class DoublyLinkedList {
             this.pre = pre;
             this.next = next;
         }
-        public Node(int data){
+
+        public Node(int data) {
             this.data = data;
             this.pre = null;
             this.next = null;
         }
 
     }
+
     private int size = 0;
     private Node head = null;
     private Node tail = null;
@@ -23,6 +25,11 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
         DoublyLinkedList linkedList = new DoublyLinkedList();
         System.out.println(linkedList);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
 
     }
 }
