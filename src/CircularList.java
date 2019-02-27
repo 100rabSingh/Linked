c66e6425bbd66c20809fe18b2a6c68c53e8c69b4
100rabSingh <100rab2000@gmail.com>
2019-02-27 11:46:55 +0530

    Circular List

--- src/CircularList.java
+++ src/CircularList.java
@@ -1,15 +1,43 @@
 public class CircularList {
-    class Node{
+    class Node {
         int data;
         Node next;
-        public Node(int data){
+
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
-        public Node(int data, Node next){
+
+        public Node(int data, Node next) {
             this.data = data;
             this.next = next;
         }
     }
 
+    int size = 0;
+    Node head = null;
+    Node tail = null;
+
+
+    public static void main(String[] args) {
+        CircularList list = new CircularList();
+        System.out.println(list);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        Node temp = this.head;
+        while (temp != null){
+            response.append(temp.data);
+            if(temp.next != null){
+                response.append("==>");
+            }
+            temp = temp.next;
+        }
+        response.append("]");
+        return response.toString();
+
+    }
 }
